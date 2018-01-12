32c6f0471ab9ce8bb709c7e96d12ddba151a5709
Andrew J Westlake <kelixes@gmail.com>
2018-01-12 01:43:28 -0600

    added .git to url

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ cargo publish --token $CARGO_TOKEN || exit 1
 
 VERSION=`cargo pkgid | sed -E 's/.*#(.*:)?(.+)/\2/'`
 git tag v$VERSION || exit 2
-git push https://awestlake87:$GH_TOKEN@github.com/awestlake87/organelle v$VERSION || exit 3
+git push https://awestlake87:$GH_TOKEN@github.com/awestlake87/organelle.git v$VERSION || exit 3
